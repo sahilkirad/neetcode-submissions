cf1fca996ed3c98cc0ff55e15f4825c12cf65548
Sahil Kirad <167863755+sahilkirad@users.noreply.github.com>
2026-06-19 10:34:43 +0530

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.arr=[]
+    
+    
+
+    def push(self, val: int) -> None:
+        self.arr.append(val)
+
+    def pop(self) -> None:
+        if self.arr:
+            self.arr.pop()
+
+    def top(self) -> int:
+        if self.arr:
+            return self.arr[-1]
+
+    def getMin(self) -> int:
+        if self.arr:
+            # min1=float('inf')
+            # for i in self.arr:
+            #     if i<min1:
+            #         min1=i
+            return min(self.arr)
